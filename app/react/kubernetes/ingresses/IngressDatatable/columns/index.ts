75953cb55a530dbb854a96c351440f77cb8b2805
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-12-27 16:22:59 +0200

    refactor(ui/datatable): remove casting of columns

--- app/react/kubernetes/ingresses/IngressDatatable/columns/index.ts
+++ app/react/kubernetes/ingresses/IngressDatatable/columns/index.ts
@@ -1,17 +1,7 @@
-import { ColumnDef } from '@tanstack/react-table';
-
-import { Ingress } from '../../types';
-
 import { name } from './name';
 import { type } from './type';
 import { namespace } from './namespace';
 import { className } from './className';
 import { ingressRules } from './ingressRules';
 
-export const columns = [
-  name,
-  namespace,
-  className,
-  type,
-  ingressRules,
-] as Array<ColumnDef<Ingress>>;
+export const columns = [name, namespace, className, type, ingressRules];
